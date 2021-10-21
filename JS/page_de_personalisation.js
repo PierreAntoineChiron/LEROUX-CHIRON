759bc306a65e70dc28a3c93c5b48098857f7e1eb
Gaelle Leroux <gaelle.leroux@cpe.fr>
2021-10-21 10:45:25 +0200

    "dynamique couleur image"

--- JS/page_de_personalisation.js
+++ JS/page_de_personalisation.js
@@ -12,6 +12,18 @@ fetch('/JS/base_de_donnees.json')
     id_images_changeante.setAttribute("src",image); // on lui demande de changer la valeur associée a src par image
 });
 
+
+function ChangeImage(element){
+  let produit_id = new URLSearchParams(window.location.search).get("id");
+  let images= base_de_donnees[produit_id].couleur[element.value]
+  id_images = document.getElementById("image_personnalisation"); 
+  id_images_changeante.setAttribute("src",images)
+
+}
+
+
+
+
 // Première façon de faire affichageImages sans le JSON en utilisant un dictionnaire
 // let id_to_path = {1:"../images/tapis_de_souris_sans_fond/Tapis_de_souris_noir_sans_fond.png" , 2:"../images/souris/Souris_noir.png", 3:"../images/clavier/Clavier_noir.png", 4:"../images/ecran/ecran.png", 5:"../images/chaises/Chaise_noir.png", 6: "../images/bureaux/Bureaux_noir.png"} // dictionnaire avec tous les liens vers les images
 //function affichageImages(){
@@ -19,3 +31,5 @@ fetch('/JS/base_de_donnees.json')
     //id_images_changeante = document.getElementById("image_personnalisation") // pont entre le js et le html, il contient tt la ligne ou il y a "images personalisation"
    // id_images_changeante.setAttribute("src",id_to_path[produit_id]) // on lui demande de changer la valeur associée a src par l'élément du dictionnaire numéro "produit_id"
 //})
+
+
