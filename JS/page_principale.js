dadf3ce39142f4b8ffd3a9530374ac9eca21fe84
Pierre-Antoine Chiron <pierreantoine.chiron@gmail.com>
2021-10-21 12:19:31 +0200

    Bouton pour remonter sur la page principale

--- JS/page_principale.js
+++ JS/page_principale.js
@@ -1,2 +1,20 @@
 fetch("header.html").then(contenu => contenu.text()).then(texte => {document.getElementById("header").innerHTML = texte;})
 fetch("footer.html").then(contenu => contenu.text()).then(texte => {document.getElementById("footer").innerHTML = texte;})
+fetch("bouton.html").then()
+
+var mybutton = document.getElementById("button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() { //permet d'afficher le bouton ssi la page on scroll plus de 20 pixel vers le bas de la page//
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+function topFunction() { //Fait remoneter tout en haut de la page//
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
