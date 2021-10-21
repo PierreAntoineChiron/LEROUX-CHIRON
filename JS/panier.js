e7377e06b758914791ced9db10301f025405907f
Pierre-Antoine Chiron <pierreantoine.chiron@gmail.com>
2021-10-21 10:51:38 +0200

    header et footer dynamiques

--- JS/panier.js
+++ JS/panier.js
@@ -7,3 +7,5 @@ var an=maintenant.getFullYear();
 
 date.setAttribute("min", "",an,"-",mois,"-",jour,"") //Bon ca fonctionne pas encore//
 
+fetch("header.html").then(contenu => contenu.text()).then(texte => {document.getElementById("header").innerHTML = texte;})
+fetch("footer.html").then(contenu => contenu.text()).then(texte => {document.getElementById("footer").innerHTML = texte;})
