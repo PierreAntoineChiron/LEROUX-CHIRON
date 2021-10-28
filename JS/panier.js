92a14546744774b1c2b57d4d8629fab81341fadc
Pierre-Antoine Chiron <pierreantoine.chiron@gmail.com>
2021-10-28 12:21:02 +0200

    Debut du local storage

--- JS/panier.js
+++ JS/panier.js
@@ -1,11 +1,9 @@
-date=document.getElementById("date")
+//date=document.getElementById("date")
 
-var maintenant=new Date();
-var jour=maintenant.getDate();
-var mois=maintenant.getMonth()+1;
-var an=maintenant.getFullYear();
+//var maintenant=new Date();
+//var jour=maintenant.getDate();
+//var mois=maintenant.getMonth()+1;
+//var an=maintenant.getFullYear();
 
-date.setAttribute("min", "",an,"-",mois,"-",jour,"") //Bon ca fonctionne pas encore//
+//date.setAttribute("min", "",an,"-",mois,"-",jour,"") //Bon ca fonctionne pas encore//
 
-fetch("header.html").then(contenu => contenu.text()).then(texte => {document.getElementById("header").innerHTML = texte;})
-fetch("footer.html").then(contenu => contenu.text()).then(texte => {document.getElementById("footer").innerHTML = texte;})
