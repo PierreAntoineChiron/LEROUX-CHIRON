827c1db6893b3801d279e1c8df58b7c52b6b47da
Gaelle Leroux <gaelleleroux974@gmail.com>
2021-11-04 12:52:53 +0100

    api marche en fonction de notre prix !!!!! mais je sais toujours pas ou on est

--- JS/page_de_personalisation.js
+++ JS/page_de_personalisation.js
@@ -9,7 +9,10 @@ fetch('/JS/base_de_donnees.json')
     id_images_changeante = document.getElementById("image_personnalisation"); // pont entre le js et le html, il contient tt la ligne ou il y a "images personalisation"
     console.log(base_de_donnees); // sert a vérifier si il trouve bien la base de donnees (visible ds l'onglet console de la page)
     let image = base_de_donnees[produit_id].couleur.noir; // on stocke le lien de l'image (c'est l'image noir) dans la variable image
+    let prixp = base_de_donnees[produit_id].prix
+    id_prix = document.getElementById("prix");
     console.log(image)
+    id_prix.innerHTML = prixp + ' €';
     id_images_changeante.setAttribute("src",image); // on lui demande de changer la valeur associée a src par image
     list=Object.keys(base_de_donnees[produit_id].couleur); // on recupere une liste avec les couleur liée à notre produit
     for (let i=1; i < list.length; i++){ // on commence à 1 car on a deja créer un bouton pour le noir dans le html et dans notre base de donnée list[0] est la couleur noir et on va jusqu'a la taille de list
@@ -32,18 +35,20 @@ function ChangeImage2(element){
 const APIKEY= "pk.eyJ1IjoiZ2FlbGxlbGVyb3V4IiwiYSI6ImNrdmd1NHl0cTVzN3QzMXF3Z29yNmdlOTUifQ.uOKemiUOXlxp59va8Q6TUw";
 
 function livraison() {
-  var address = document.getElementById("truc").value;
-  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${APIKEY}`)
+  var address = document.getElementById("truc").value; // recupere adresse de l'input
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${APIKEY}`) // on transforme l'adresse en longitude et lattitude
       .then(res => res.json())
       .then(data => {
-          coord = data.features[0].center;
-          long=coord[0];
-          lat=coord[1];
-          fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${encodeURIComponent(long)}%2C${encodeURIComponent(lat)}%3B4.8593999%2C45.7611438?alternatives=true&geometries=geojson&steps=true&access_token=${APIKEY}`)
+          coord = data.features[0].center; // on recupere coordonne de l'adresse
+          long=coord[0]; // longitude  de l'adresse
+          lat=coord[1]; // l'attitude de l'adresse
+          fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${encodeURIComponent(long)}%2C${encodeURIComponent(lat)}%3B4.8593999%2C45.7611438?alternatives=true&geometries=geojson&steps=true&access_token=${APIKEY}`) // on recupere distance entre nos 2 points
           .then(response => response.json() 
           .then(data=>{
               if (data.routes[0].distance >20000){
-              document.querySelector('#prix').innerHTML=(199+ 5+0.07*data.routes[0].distance/1000) + '€' ;
+              let produit_id = new URLSearchParams(window.location.search).get("id");
+              let prixp = base_de_donnees[produit_id].prix;
+              document.querySelector('#prix').innerHTML=(prixp+ 5+0.07*data.routes[0].distance/1000) + '€' ;
               document.querySelector('#phrase').innerHTML="Prix de la livraison : " + (5+0.07*data.routes[0].distance/1000) + "€"; 
               }
               else{
