6f8da999f09afb51972142cbadde5231106e7256
Gaelle Leroux <gaelleleroux974@gmail.com>
2021-11-04 14:51:54 +0100

    page de contact

--- JS/page_contact.js
+++ JS/page_contact.js
@@ -1,2 +1,6 @@
+
+
+
+
 fetch("header.html").then(contenu => contenu.text()).then(texte => {document.getElementById("header").innerHTML = texte;})
 fetch("footer.html").then(contenu => contenu.text()).then(texte => {document.getElementById("footer").innerHTML = texte;})
