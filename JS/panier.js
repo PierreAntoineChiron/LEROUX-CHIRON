70cd84ebfee04681e711cd5b2e117389fa6f7af1
Gaelle Leroux <gaelleleroux974@gmail.com>
2021-11-08 22:34:10 +0100

    recapitulatif commande

--- JS/panier.js
+++ JS/panier.js
@@ -2,7 +2,25 @@ let base_de_donnees = {};
 fetch('/JS/base_de_donnees.json')
 .then(function(response) {
     return response.json();
-})    
+})   
+
+
+.then(function(json) {
+    base_de_donnees = json // on affecte la base de donne a la variable json
+    var template=document.getElementById("produit"); // on lui indique sur quel template dans notre html on va travailler
+    for (let i=0; i < localStorage.length; i++) {
+        arraytemp = localStorage.getItem(i).split(",");
+        let clone = document.importNode(template.content, true);  // copier coller du cours
+        newContent = clone.firstElementChild.innerHTML // copier coller du cours
+            .replace(/{{prix}}/g,base_de_donnees[arraytemp[0]].prix + "€") // on remplace idée du produit qui est ds html par son id, c'est pr le lien vers la page de personalisation
+            .replace(/{{image}}/g,base_de_donnees[arraytemp[0]].couleur[arraytemp[4]]) // on affiche l'image
+            .replace(/{{titre}}/g,base_de_donnees[arraytemp[0]].titre) // j'ai mis un titre à nos accesoire mais on pourra l'enlever ou mettre un commentaire ou le prix
+            .replace(/{{quantite}}/g,"quantité : " + arraytemp[3])
+        clone.firstElementChild.innerHTML = newContent // copier coller du cours
+        document.body.appendChild(clone); // copier coller du cours
+              
+    }
+ 
     
 const APIKEY= "pk.eyJ1IjoiZ2FlbGxlbGVyb3V4IiwiYSI6ImNrdmd1NHl0cTVzN3QzMXF3Z29yNmdlOTUifQ.uOKemiUOXlxp59va8Q6TUw";
 
@@ -33,18 +51,18 @@ function livraison() {
               }));
       })};
 
-      function prixtotal() {
-        prixtotaltest = 0
-        valeurfdl = parseInt(document.getElementById("phrase").innerHTML)
-        for (let i=0 ; i <localStorage.length -7 ; i++) {
-            arraytemp = localStorage.getItem(i).split(",");
-            prixtemp = arraytemp[2]
-            nbtemp = arraytemp[3]
-            prixtotaltest = prixtotaltest + prixtemp*nbtemp;
-        }
-        let prixt = document.getElementById("prixtotal");
-        prixt.innerHTML = prixtotaltest + valeurfdl;
+function prixtotal() {
+    prixtotaltest = 0
+    valeurfdl = parseInt(document.getElementById("phrase").innerHTML)
+    for (let i=0 ; i <localStorage.length -7 ; i++) {
+        arraytemp = localStorage.getItem(i).split(",");
+        prixtemp = arraytemp[2]
+        nbtemp = arraytemp[3]
+        prixtotaltest = prixtotaltest + prixtemp*nbtemp;
     }
+    let prixt = document.getElementById("prixtotal");
+    prixt.innerHTML = prixtotaltest + valeurfdl;
+}
 
 function store(){
         var inputFirstName = document.getElementById("firstname");
@@ -63,4 +81,4 @@ function store(){
         localStorage.setItem("Code Postal", inputCodePostal.value);
         localStorage.setItem("Date de livraison", inputDate.value);
         
-}
+}})
